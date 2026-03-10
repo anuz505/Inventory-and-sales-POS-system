f67decc5498985467e896f88b661586aa0bb67c8
anuz505 <anujb364@gmail.com>
2026-03-10 09:44:30 +0545

    postgres test setting

--- internship_task/test_settings.py
+++ internship_task/test_settings.py
@@ -5,3 +5,24 @@ CACHES = {"default": {"BACKEND": "django.core.cache.backends.locmem.LocMemCache"
 
 CELERY_TASK_ALWAYS_EAGER = True
 CELERY_TASK_EAGER_PROPAGATES = True
+
+# Override database settings for tests
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "inventory-sales-db",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
+}
+
+# $ docker run -d \
+#   --name inventory_sale_db \
+#   -e POSTGRES_USER=postgres \
+#   -e POSTGRES_PASSWORD=postgres \
+#   -e POSTGRES_DB=inventory-sales-db \
+#   -p 5432:5432 \
+#   -v postgres_data:/var/lib/postgresql/data \
+#   postgres:15-alpine
